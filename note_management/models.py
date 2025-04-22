88f4298f6e3549f28120c1e8fea0da0ece3b37d2
eieihtwesan <40685545@live.napier.ac.uk>
2025-04-22 23:36:34 +0630

    create Notes, Summary models

--- note_management/models.py
+++ note_management/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from core.models import User
+from uuid import uuid4
 
 # Create your models here.
+class Notes(models.Model):
+      
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='notes')
+    
+    def __str__(self):
+        return f"{self.title} written by {self.user.username}"
+    
+class Summary(models.Model):
+    
+    note = models.OneToOneField(
+        Notes,
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Summary for {self.note.title}"
