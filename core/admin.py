fba06e0a4788de776a8d944459b81ad1715d9771
eieihtwesan <40685545@live.napier.ac.uk>
2025-04-22 23:15:32 +0630

    fix mistakes

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from core.models import User, Notes, Summary
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Notes)
-admin.site.register(Summary)
+
 
--- core/models.py
+++ core/models.py
@@ -11,30 +11,3 @@ class User(AbstractUser):
             self.email = self.email.lower()
         super().save(*args, **kwargs)
         
-
-class Notes(models.Model):
-      
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    is_public = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name='notes')
-    
-    def __str__(self):
-        return f"{self.title} written by {self.user.username}"
-    
-class Summary(models.Model):
-    
-    note = models.OneToOneField(
-        Notes,
-        on_delete=models.CASCADE,
-    )
-    content = models.TextField()
-
-    def __str__(self):
-        return f"Summary for {self.note.title}"
