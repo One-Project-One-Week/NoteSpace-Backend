b3d03f706f109b16a64624d0a548f909681457ee
eieihtwesan <40685545@live.napier.ac.uk>
2025-04-22 22:55:29 +0630

    Notes and Summary models create

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import User, Notes, Summary
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Notes)
+admin.site.register(Summary)
+
