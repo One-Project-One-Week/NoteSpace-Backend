9af07425a3fcdd7025295cf9b89a6059602f4ac7
Direwen <direwensik@gmail.com>
2025-04-22 15:33:39 +0630

    Init commit

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        if self.email:
+            self.email = self.email.lower()
+        super().save(*args, **kwargs)
