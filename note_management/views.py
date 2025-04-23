6ef7856024bcb10110aea9bc8767319af1bbb7b6
Direwen <direwensik@gmail.com>
2025-04-24 00:21:24 +0630

    Added a custom endpoint for notes generator

--- note_management/views.py
+++ note_management/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from note_management.serializers import NoteSerializer
+from rest_framework.filters import SearchFilter
+from rest_framework.validators import ValidationError
+from note_management.serializers import NoteSerializer, UploadFileSerializer
 from note_management.models import Note
 from .permissions import IsNoteOwner
 from .utils.summarizer.summarizer_util import summarize
-from rest_framework.filters import SearchFilter
+from .utils.generator.notes_generator import generate_notes
+from .utils.processor.text_extractor import extract_texts_from_files
+from .utils.processor.llm_input_preprocessor import tokenize_and_split_text
 
 class NotesViewSet(viewsets.ModelViewSet):
     serializer_class = NoteSerializer
@@ -51,10 +55,49 @@ class NotesViewSet(viewsets.ModelViewSet):
             }
         )
         
+    @action(detail=False, methods=['POST'], url_path='upload', serializer_class=UploadFileSerializer)
+    def generate_notes(self, request):
         
+        serializer = UploadFileSerializer(data=request.data)
         
+        if not serializer.is_valid():
+            raise ValidationError(serializer.errors)
         
-    
-    
-    
-    
+        uploaded_file = serializer.validated_data["file"]
+        
+        try:
+            
+            extracted_content = extract_texts_from_files(uploaded_file)
+                
+            content_chunks = tokenize_and_split_text(extracted_content)
+            
+            #generate notes for each chunk
+            notes = [generate_notes(model="qwen/qwen-2.5-7b-instruct:free", content=chunk) for chunk in content_chunks]
+            
+            #combine notes into one by a new line
+            note_data = {
+                "title": uploaded_file.name.split(".")[0],
+                "content": "\n".join(notes),
+            }
+            
+            serializer = NoteSerializer(data=note_data)
+            
+            if not serializer.is_valid():
+                return Response(
+                    {
+                        "error": f"Error validating note data: {serializer.errors}"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            serializer.save(user=self.request.user)
+            
+            return Response({"response": "Note Generated Successfully"})    
+        
+        except Exception as err:
+            return Response(
+                {
+                    "error": f"Error occured while reading the file: {err}"
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
