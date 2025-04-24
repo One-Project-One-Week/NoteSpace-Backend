55aec6dd22121630a23b2286b36065d8dae99af1
Khant Thit Oo <127503879+travthegoat@users.noreply.github.com>
2025-04-24 21:54:21 +0630

    Changed Note Generator Model To Groq

--- note_management/views.py
+++ note_management/views.py
@@ -86,7 +86,7 @@ class NotesViewSet(viewsets.ModelViewSet):
             content_chunks = tokenize_and_split_text(extracted_content)
             
             #generate notes for each chunk
-            notes = [generate_notes(model="qwen/qwen-2.5-7b-instruct:free", content=chunk) for chunk in content_chunks]
+            notes = [generate_notes(content=chunk) for chunk in content_chunks]
             
             #combine notes into one by a new line
             note_data = {
