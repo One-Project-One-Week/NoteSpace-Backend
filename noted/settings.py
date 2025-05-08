601242b017fb1a808f45eb4659d4f5754b5cee7b
Direwen <direwensik@gmail.com>
2025-05-08 11:45:41 +0630

    Added Caching with Redis

--- noted/settings.py
+++ noted/settings.py
@@ -162,3 +162,13 @@ DJOSER = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
